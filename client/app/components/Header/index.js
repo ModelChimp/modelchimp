5552782851bb120b763bb28385cc2a9c5160eacd
Samir Madhavan <samir.madhavan@gmail.com>
2019-01-04 13:01:13 +0530

    Fix top and side navigation

--- client/app/components/Header/index.js
+++ client/app/components/Header/index.js
@@ -12,13 +12,12 @@ import { Layout, Menu } from 'antd';
 class Header extends React.Component {
   render() {
     return (
-      <Layout.Header>
-        <div className="logo" />
+      <Layout.Header style={{position:'fixed', zIndex:300, width:'100vw' }}>
         <Menu
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
+          style={{ lineHeight: '64px'}}
         >
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
@@ -32,3 +31,4 @@ class Header extends React.Component {
 export default Header;
 
 //https://docs.modelchimp.com/logo.png
+//<div className="logo" />
--- client/app/containers/ExperimentDetailPage/index.js
+++ client/app/containers/ExperimentDetailPage/index.js
@@ -15,14 +15,19 @@ import {makeSelectExperimentDetail,
 import reducer from './reducer';
 import saga from './saga';
 import Header from 'components/Header';
-import { Breadcrumb } from 'antd';
+import {
+  Layout, Menu, Breadcrumb, Icon,
+} from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ExpIdBlock from 'components/ExpIdBlock';
-import { Layout } from 'antd';
 import Content from 'components/Content';
+
+
 
 import {loadExperimentDetailAction} from './actions';
 
+const { SubMenu } = Menu;
+const {  Sider } = Layout;
 /* eslint-disable react/prefer-stateless-function */
 export class ExperimentDetailPage extends React.Component {
   componentDidMount() {
@@ -38,20 +43,51 @@ export class ExperimentDetailPage extends React.Component {
           <meta name="description" content="Description of ExperimentDetail" />
         </Helmet>
         <Header />
-        <Content>
-        <h1></h1>
-        <div style={{marginTop:"50px"}}>
-          <Breadcrumb separator=">">
-            <Breadcrumb.Item><a href="/projects">Projects</a></Breadcrumb.Item>
-            <Breadcrumb.Item><a href={`/experiment-list/${this.props.experiment.project}`}>{this.props.experiment.project_name}</a></Breadcrumb.Item>
-            <Breadcrumb.Item><a href={`/experiment-detail/${this.props.experiment.id}`}>{this.props.experimentName}</a></Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{marginTop:"20px"}}>
-            <h2 style={{display:"inline"}}>Experiment Name: {this.props.experimentName}</h2>
-            <ExpIdBlock expId={this.props.experimentId} sExpId={this.props.shortExperimentId} style={{float:"right"}} />
+          <Layout>
+          <Sider width={200} style={{ background: '#fff', position:'fixed',marginTop:64, zIndex:200, height:'100vh' }}>
+                  <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    style={{ height: '100%', borderRight: 0 }}
+                  >
+                    <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                      <Menu.Item key="1">option1</Menu.Item>
+                      <Menu.Item key="2">option2</Menu.Item>
+                      <Menu.Item key="3">option3</Menu.Item>
+                      <Menu.Item key="4">option4</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                      <Menu.Item key="5">option5</Menu.Item>
+                      <Menu.Item key="6">option6</Menu.Item>
+                      <Menu.Item key="7">option7</Menu.Item>
+                      <Menu.Item key="8">option8</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                      <Menu.Item key="9">option9</Menu.Item>
+                      <Menu.Item key="10">option10</Menu.Item>
+                      <Menu.Item key="11">option11</Menu.Item>
+                      <Menu.Item key="12">option12</Menu.Item>
+                    </SubMenu>
+                  </Menu>
+                </Sider>
+        <Content style={{marginLeft:200}}>
+          <h1></h1>
+
+          <div style={{marginTop:"50px"}}>
+            <Breadcrumb separator=">">
+              <Breadcrumb.Item><a href="/projects">Projects</a></Breadcrumb.Item>
+              <Breadcrumb.Item><a href={`/experiment-list/${this.props.experiment.project}`}>{this.props.experiment.project_name}</a></Breadcrumb.Item>
+              <Breadcrumb.Item><a href={`/experiment-detail/${this.props.experiment.id}`}>{this.props.experimentName}</a></Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{marginTop:"20px"}}>
+              <h2 style={{display:"inline"}}>Experiment Name: {this.props.experimentName}</h2>
+              <ExpIdBlock expId={this.props.experimentId} sExpId={this.props.shortExperimentId} style={{float:"right"}} />
+            </div>
           </div>
-        </div>
         </Content>
+      </Layout>
+
       </Layout>
     );
   }
