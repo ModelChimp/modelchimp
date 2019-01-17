edb75688ab7532ef10b216dfb9fff5685892a480
Samir Madhavan <samir.madhavan@gmail.com>
2019-01-17 17:15:34 +0530

    Remove unnecessary content

--- client/app/containers/ExperimentList/ProjectSetting/DetailEditForm.js
+++ client/app/containers/ExperimentList/ProjectSetting/DetailEditForm.js
@@ -7,29 +7,6 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { makeSelectProjectDetail } from 'containers/ProjectDetail/selectors';
 
-const residences = [{
-  value: 'zhejiang',
-  label: 'Zhejiang',
-  children: [{
-    value: 'hangzhou',
-    label: 'Hangzhou',
-    children: [{
-      value: 'xihu',
-      label: 'West Lake',
-    }],
-  }],
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-  children: [{
-    value: 'nanjing',
-    label: 'Nanjing',
-    children: [{
-      value: 'zhonghuamen',
-      label: 'Zhong Hua Men',
-    }],
-  }],
-}];
 
 class RegistrationForm extends React.Component {
   state = {
