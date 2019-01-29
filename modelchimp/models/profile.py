b783305070a7efefa9656cad387fedc355e86d69
Samir Madhavan <samir.madhavan@gmail.com>
2019-01-29 15:10:04 +0530

    Add default first name to Unknown

--- modelchimp/models/profile.py
+++ modelchimp/models/profile.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     user   = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.ImageField(null=True, upload_to='profile/')
     avatar_url = models.URLField(null=True)
-    first_name = models.CharField(max_length=200, blank=True, default='')
+    first_name = models.CharField(max_length=200, blank=True, default='Unknown')
     last_name = models.CharField(max_length=200, blank=True, default='')
 
     def __str__(self):
