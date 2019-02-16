fb236d2809c46569632efd39ca2248012dc0a6d3
Samir Madhavan <samir.madhavan@gmail.com>
2019-02-16 21:39:28 +0530

    Add bank the password reset urls

--- settings/urls.py
+++ settings/urls.py
@@ -9,11 +9,24 @@ from rest_framework.authtoken import views
 from django.views.generic.base import TemplateView
 from django.views.decorators.cache import cache_control
 
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetCompleteView,
+)
 
 urlpatterns = [
     url(r'^api/', include('modelchimp.api_urls')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^hq/', admin.site.urls),
+
+    # Password reset views
+    url(r'^password_reset/$', PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
