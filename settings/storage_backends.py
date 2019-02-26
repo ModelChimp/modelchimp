0f5f072b144f49a1f277dd492e257fa26d33902e
Samir Madhavan <samir.madhavan@gmail.com>
2019-02-26 19:00:36 +0530

    Correct class S3MediaStorage

--- settings/storage_backends.py
+++ settings/storage_backends.py
@@ -1,7 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-class MediaStorage(S3Boto3Storage):
+class S3MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'private'
     file_overwrite = False
