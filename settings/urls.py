c0fe502e380d32f42ed752cd3b6e1fd076a62d51
Samir Madhavan <samir.madhavan@gmail.com>
2019-03-07 23:48:56 +0530

    Add static url along with media url

--- settings/urls.py
+++ settings/urls.py
@@ -29,3 +29,5 @@ urlpatterns = [
 
 urlpatterns = urlpatterns  + static(settings.MEDIA_URL,
                                 document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns  + static(settings.STATIC_URL,
+                                document_root=settings.STATIC_ROOT)
