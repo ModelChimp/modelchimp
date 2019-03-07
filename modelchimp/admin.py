5aa1ec37c7f9d3db5ee305fe830327f486d37ce5
Samir Madhavan <samir.madhavan@gmail.com>
2019-03-08 00:03:31 +0530

    Add user to the admin page

--- modelchimp/admin.py
+++ modelchimp/admin.py
@@ -3,6 +3,7 @@ from .models.machinelearning_model import MachineLearningModel
 from .models.comment import Comment
 from .models.profile import Profile
 from .models.invitation import Invitation
+from .models.user import User
 from modelchimp.models.experiment_custom_object import ExperimentCustomObject
 from modelchimp.models.experiment_image import ExperimentImage
 
@@ -13,3 +14,4 @@ admin.site.register(Invitation)
 admin.site.register(ExperimentCustomObject)
 admin.site.register(ExperimentImage)
 admin.site.register(Profile)
+admin.site.register(User)
