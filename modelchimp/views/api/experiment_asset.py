3bfb07277905a9055c9ec65a2070c30d2b4f4ba8
Samir Madhavan <samir.madhavan@gmail.com>
2019-03-08 20:32:15 +0530

    Create API for asset meta fields

--- modelchimp/views/api/experiment_asset.py
+++ modelchimp/views/api/experiment_asset.py
@@ -2,9 +2,12 @@ from rest_framework import generics
 
 from modelchimp.serializers.experiment_asset import ExperimentAssetSerializer
 from modelchimp.models.experiment_asset import ExperimentAsset
+from modelchimp.utils.data_utils import execute_query
 
 from modelchimp.api_permissions import HasProjectMembership
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 
 class ExperimentAssetAPI(generics.ListCreateAPIView):
@@ -15,3 +18,25 @@ class ExperimentAssetAPI(generics.ListCreateAPIView):
     def filter_queryset(self, queryset):
         mid = self.kwargs.get('model_id')
         return queryset.filter(ml_model=mid)
+
+
+@api_view(['GET'])
+@permission_classes((HasProjectMembership, IsAuthenticated))
+def get_asset_meta_fields(request, model_id):
+	'''
+    List the meta fields of assets in an experiment
+	'''
+
+	query = '''
+	select distinct json_object_keys(meta_dict::json) as name
+	from modelchimp_experimentasset me
+	where json_typeof(meta_dict::json) = 'object'
+	and me.ml_model_id = %s
+	'''
+
+	query = query % (
+	    model_id,
+	)
+	result = execute_query(query)
+    
+	return Response(result)
