6f420483d380542eca853f3e2a0ecef830bb5965
Samir Madhavan <samir.madhavan@gmail.com>
2019-05-07 15:59:11 +0530

    Bring back NotFoundPage

---
+++ client/app/containers/NotFoundPage/messages.js
@@ -0,0 +1,15 @@
+/*
+ * NotFoundPage Messages
+ *
+ * This contains all the text for the NotFoundPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export const scope = 'boilerplate.containers.NotFoundPage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'Page not found.',
+  },
+});
