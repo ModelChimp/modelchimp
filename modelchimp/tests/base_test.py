5adbebc32838ed86bb67544f4570ddcbec01b155
Samir Madhavan <samir.madhavan@gmail.com>
2019-06-18 18:01:31 +0530

    Update the BaseTest

--- modelchimp/tests/base_test.py
+++ modelchimp/tests/base_test.py
@@ -9,13 +9,16 @@ from modelchimp.factories.factory_experiment import ExperimentFactory
 
 
 class BaseTest(TestCase):
+    uname = 'admin@modelchimp.com'
+    password = 'modelchimp123'
+
     def setUp(self):
         self._data_setup()
         self.client = APIClient()
-        self.login = self.client.login(username='admin@modelchimp.com', password='modelchimp123')
+        self.login = self.client.login(username=self.uname, password=self.password)
 
     def _data_setup(self):
-        self.user = UserFactory(email='admin@modelchimp.com')
+        self.user = UserFactory(email=self.uname)
         self.profile = ProfileFactory(user=self.user)
         self.project = ProjectFactory(user=self.user)
         self.membership = MembershipFactory(user=self.user, project=self.project)
