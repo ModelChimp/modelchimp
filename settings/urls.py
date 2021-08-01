48668d0f73025b2cc967006b3193b67aaf970ad7
Karthik Manjunath <karthik666manjunath@gmail.com>
2021-08-01 12:41:57 +0530

    Update urls.py

--- settings/urls.py
+++ settings/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from django.contrib import admin 
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,4 +27,4 @@ urlpatterns = [
 urlpatterns = urlpatterns  + static(settings.MEDIA_URL,
                                 document_root=settings.MEDIA_ROOT)
 urlpatterns = urlpatterns  + static(settings.STATIC_URL,
-                                document_root=settings.STATIC_ROOT)
+                                document_root=settings.STATIC_ROOT) 
